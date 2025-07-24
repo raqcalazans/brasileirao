768d868d7418e900804160e5cad7a3d4519d657e
Raquel Calazans <raqcalazans@gmail.com>
2025-07-23 22:47:10 -0300

    feat(ios): Creating tests files

---
+++ ios-app/Brasileirao/BrasileiraoTests/BrasileiraoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrasileiraoTests.swift
+//  BrasileiraoTests
+//
+//  Created by Raquel Calazans on 23/07/25.
+//
+
+import Testing
+@testable import Brasileirao
+
+struct BrasileiraoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
