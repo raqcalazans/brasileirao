dc941434c330c61313d90890220f4dadca1d5c4e
Raquel Calazans <raqcalazans@gmail.com>
2025-07-23 22:35:42 -0300

    feat(api): Build repository and service layers

--- api/brasileirao-api/src/main/java/com/selecaoglobocom/brasileirao_api/repository/TeamRepository.java
+++ api/brasileirao-api/src/main/java/com/selecaoglobocom/brasileirao_api/repository/TeamRepository.java
@@ -1,10 +1,9 @@
 package com.selecaoglobocom.brasileirao_api.repository;
 
-import com.selecaoglobocom.brasileirao_api.model.Team;
+import com.selecaoglobocom.brasileirao_api.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
-    // Já fornece métodos como findById(), findAll(), save(), delete(), etc.
+public interface TeamRepository extends JpaRepository<Team, Integer> {
 }
